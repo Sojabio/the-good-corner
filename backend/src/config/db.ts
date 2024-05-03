0270c3e48a71a3b7c840a5c0670449498aef00f7
Sojabio <solene.jabaud@gmail.com>
2024-05-03 09:41:41 +0200

    first commit

---
+++ backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import { Category } from "../entities/category";
+import { Ad } from "../entities/ad";
+import { DataSource } from "typeorm";
+import { Tag } from "../entities/tag";
+
+export const dataSource = new DataSource({
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: 'postgres',
+  password: 'example',
+  database: "postgres",
+  synchronize: true,
+  logging: ["error", "query"],
+  entities: [Ad, Category, Tag],
+});
